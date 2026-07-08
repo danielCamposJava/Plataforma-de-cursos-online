ebc8942bfc6c3e8299854daa9f3e4bd2babe8b1c
Daniel Anderson <dev.danielanderson@outlook.com>
2026-07-08 11:20:52 -0400

    feat: criando camada de service

--- src/main/java/plataformadecurso/demo/User/UserService/UserService.java
+++ src/main/java/plataformadecurso/demo/User/UserService/UserService.java
@@ -1,4 +1,59 @@
 package plataformadecurso.demo.User.UserService;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import plataformadecurso.demo.User.DTO.UserRequestDTO;
+import plataformadecurso.demo.User.DTO.UserResponseDTO;
+import plataformadecurso.demo.User.UserEntity.UserEntity;
+import plataformadecurso.demo.User.UserRepository.UserRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+
+        if( userRepository.existsById(UUID.fromString(userRequestDTO.email()))){
+             throw  new RuntimeException("Email ja existente");
+         }
+
+         UserEntity userEntity = new UserEntity();
+
+         userEntity.setName(userRequestDTO.name());
+         userEntity.setEmail(userRequestDTO.email());
+         userEntity.setPassword(passwordEncoder.encode(userRequestDTO.password()));
+
+         UserEntity saved = userRepository.save(userEntity);
+         return  UserResponseDTO.fromEntity(saved);
+
+    }
+
+   public List<UserResponseDTO> findAllUser(){
+        return  userRepository.findAll().stream().map(
+                UserResponseDTO ::fromEntity
+        ).toList();
+   }
+
+   public UserResponseDTO updateUser(UUID id , UserRequestDTO userRequestDTO) {
+
+       UserEntity entity = userRepository.findById(id)
+               .orElseThrow(() -> new RuntimeException("User não econtrado"));
+
+       UserEntity userEntity = new UserEntity();
+       userEntity.setName(userRequestDTO.name());
+       userEntity.setEmail(userRequestDTO.email());
+       userEntity.setPassword(passwordEncoder.encode(userRequestDTO.password()));
+
+       UserEntity saved = userRepository.save(userEntity);
+       return UserResponseDTO.fromEntity(saved);
+   }
 }
