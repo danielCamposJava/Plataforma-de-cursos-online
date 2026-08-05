26e93bed0df153767e40d14ae936d0ded87e197c
Daniel Anderson <dev.danielanderson@outlook.com>
2026-08-05 09:33:30 -0400

    criando case de test

--- src/test/java/plataformadecurso/demo/Security/JwtServiceTest.java
+++ src/test/java/plataformadecurso/demo/Security/JwtServiceTest.java
@@ -1,16 +1,49 @@
 package plataformadecurso.demo.Security;
 
+import io.jsonwebtoken.JwtException;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class JwtServiceTest {
 
+    private final JwtService jwtService = new JwtService();
+
     @Test
-    void generateToken() {
+    void shouldgenerateToken() {
+
+    String token = jwtService.generateToken(
+            "test@email.com"
+    );
+
+    assertNotNull(token);
+    assertFalse(token.isEmpty());
+
     }
 
     @Test
-    void extractEmail() {
+    void shouldextractEmail() {
+
+        String email = "teste@gemail.com";
+
+        String token = jwtService.generateToken(email);
+
+        String extractedEmail = jwtService.extractEmail(token);
+
+        assertEquals(email, extractedEmail);
+
+
+    }
+
+    @Test
+    void soulThrowsExceptionWhenTokenIsInvalid() {
+
+        String  invalidToken =
+                "token.inavlido.aqui";
+
+        assertThrows(
+                JwtException.class,
+                () -> jwtService.extractEmail(invalidToken)
+        );
+
     }
 }
